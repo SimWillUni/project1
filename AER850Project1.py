a7116d4e0154e3cc66315304796fcead7b9f0818
SimWillUni <simon.willemsma@torontomu.ca>
2024-10-18 00:15:02 -0400

    Changed Location of Data Split
    Moved train/test split to step 4 and update data visualization to include the entire dataset instead of just train data

--- AER850Project1.py
+++ AER850Project1.py
@@ -23,8 +23,44 @@ df = pd.read_csv('data/Project_1_Data.csv')
 
 ''' step 2 '''
 
+# data visualization
 
-# in order to avoid data leaks during visualization, the train-test split is done now
+fig, axes = plt.subplots(2, 2)
+
+axes[0, 0].hist(df['Step'], bins=12)
+axes[0, 0].set_title('Step Values')
+axes[0, 1].hist(df['X'])
+axes[0, 1].set_title('X Values')
+axes[1, 0].hist(df['Y'])
+axes[1, 0].set_title('Y Values')
+axes[1, 1].hist(df['Z'])
+axes[1, 1].set_title('Z Values')
+
+plt.tight_layout()
+plt.show()
+
+fig = plt.figure()
+
+ax = fig.add_subplot(projection='3d')
+scatter_plot = ax.scatter(df['X'],df['Y'],df['Z'],c=df['Step'],cmap='winter_r')
+cbar = plt.colorbar(scatter_plot)
+cbar.set_label("Step")
+
+plt.show()
+
+
+''' step 3 '''
+
+
+sns.heatmap(np.abs(df.corr()), cmap="winter_r")
+plt.show()
+print("\nThe Correlation Matrix for all four variables is as follows:\n")
+print(df.corr(method='pearson'))
+
+
+''' step 4 '''
+
+# train/test split
 
 my_splitter = StratifiedShuffleSplit(n_splits = 1, test_size = 0.2, random_state = 777)
 for train_index, test_index in my_splitter.split(df, df["Step"]):
@@ -35,44 +71,6 @@ X_train = strat_df_train.drop("Step", axis = 1)
 y_train = strat_df_train["Step"]
 X_test = strat_df_test.drop("Step", axis = 1)
 y_test = strat_df_test["Step"]
-
-# data visualization
-
-fig, axes = plt.subplots(2, 2)
-
-axes[0, 0].hist(y_train, bins=12)
-axes[0, 0].set_title('Step Values')
-axes[0, 1].hist(X_train['X'])
-axes[0, 1].set_title('X Values')
-axes[1, 0].hist(X_train['Y'])
-axes[1, 0].set_title('Y Values')
-axes[1, 1].hist(X_train['Z'])
-axes[1, 1].set_title('Z Values')
-
-plt.tight_layout()
-plt.show()
-
-fig = plt.figure()
-
-ax = fig.add_subplot(projection='3d')
-scatter_plot = ax.scatter(X_train['X'],X_train['Y'],X_train['Z'],c=y_train,cmap='winter_r')
-cbar = plt.colorbar(scatter_plot)
-cbar.set_label("Step")
-
-plt.show()
-
-
-''' step 3 '''
-
-
-sns.heatmap(np.abs(strat_df_train.corr()), cmap="winter_r")
-plt.show()
-print("\nThe Correlation Matrix for all four variables is as follows:\n")
-print(strat_df_train.corr(method='pearson'))
-
-
-''' step 4 '''
-
 
 # Support Vector Machine
 
